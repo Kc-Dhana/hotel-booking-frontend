dcf7caccbceb67cbfa716097bdb425fd51da27e4
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-11-30 17:17:48 +0530

    design login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,7 +1,27 @@
+import "./login.css";
 export default function LoginPage() {
     return (
-        <div className="w-full h-[100vh] bg-blue-600 ">
-            <h1>login</h1>
+        <div className="w-full h-[100vh] pic-bg flex justify-center items-center">
+            <div className="w-[400px] h-[400px] backdrop-blur-md
+            rounded-lg flex flex-col items-center relative justify-center">
+
+                <h1 className="text-3xl p-[15px] text-white text-center absolute top-[40px]">
+                    Login
+                </h1>
+
+                <input type="text" placeholder="Enter your email" className="w-[80%] h-[40px]
+                bg-[#00000000] border-[2px] text-white placeholder:text-white
+                px-[5px] mb-[20px]"/>
+
+                <input type="password" placeholder="Enter your password" className="w-[80%] h-[40px]
+                bg-[#00000000] border-[2px] text-white placeholder:text-white
+                px-[5px] mb-[10px]"/>
+
+                <button className="w-[80%] h-[40px] bg-red-500  text-white
+                hover:bg-white hover:text-black absolute bottom-[40px]">Login</button>
+
+            </div>
+            
         </div>
-    );
+    )
 }
