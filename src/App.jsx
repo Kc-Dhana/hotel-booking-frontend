e5e2eecfd356d6b0a770175dcb911ae1f4712072
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-11-30 16:30:38 +0530

    create login route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css"
 import HomePage from "./pages/client-page/homePage";
 import AdminPage from "./pages/admin-page/admin";
 import TestComponent from "./components/test/test";
+import LoginPage from "./pages/login/login";
 function App() {
   
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes path="/*">
 
         <Route path="/admin/*" element={<AdminPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/test" element={<TestComponent />} />
         <Route path="/*" element={<HomePage />} />
         
